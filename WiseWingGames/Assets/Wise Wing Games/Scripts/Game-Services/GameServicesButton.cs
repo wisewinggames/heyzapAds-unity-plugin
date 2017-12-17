72145f748c372eb7aa6ae4cf6c982d0adababcad
Salman Zafar <entityparadigm99@gmail.com>
2017-12-18 00:11:09 +0500

    created GSButton script

---
+++ WiseWingGames/Assets/Wise Wing Games/Scripts/Game-Services/GameServicesButton.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace WiseWingGames
+{
+	
+
+	public class GameServicesButton : MonoBehaviour
+	{
+
+		Button _button;
+
+		public enum GS_Button
+		{
+			ShowLeaderboard,
+			ShowAchievements,
+			SignIn,
+			SignOut
+		}
+
+		public GS_Button buttonType;
+
+
+		void Start ()
+		{
+			_button = GetComponent<Button> ();		
+
+			_button.onClick.AddListener (delegate() {});
+		
+		}
+
+
+		void OnClickButton(){
+			switch (buttonType) {
+
+			case GS_Button.ShowLeaderboard:
+				GameServicesManager.instance.ShowLeaderboardUI ();
+				break;
+
+			case GS_Button.ShowAchievements:
+				GameServicesManager.instance.ShowAchievementUI ();
+				break;
+
+			case GS_Button.SignIn:
+				GameServicesManager.instance.SignIn ();
+				break;
+
+			case GS_Button.SignOut:
+				GameServicesManager.instance.SignOut ();
+				break;
+
+			}
+		}
+
+	}
+}
