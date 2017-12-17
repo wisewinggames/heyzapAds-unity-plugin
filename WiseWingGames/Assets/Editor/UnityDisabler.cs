2dbf2259301f4a937f2069db7ab939be2f027075
Salman Zafar <entityparadigm99@gmail.com>
2017-12-17 17:54:32 +0500

    disabled unity ads auto-init

---
+++ WiseWingGames/Assets/Editor/UnityDisabler.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEditor;
+using UnityEditor.Advertisements;
+
+public class UnityDisabler : Editor {
+
+	// Use this for initialization
+	void Awake () {
+		AdvertisementSettings.initializeOnStartup = false;
+	}
+
+}
