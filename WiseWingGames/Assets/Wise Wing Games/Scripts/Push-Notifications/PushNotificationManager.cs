7537dbc987bb3474bb21194eb50c44e310c862b1
Salman Zafar <entityparadigm99@gmail.com>
2017-12-20 01:27:55 +0500

    added notifications
    working ok

---
+++ WiseWingGames/Assets/Wise Wing Games/Scripts/Push-Notifications/PushNotificationManager.cs
@@ -0,0 +1,56 @@
+﻿//https://documentation.onesignal.com/docs/unity-sdk
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OneSignalPush;
+
+namespace WiseWingGames
+{
+	
+
+	public class PushNotificationManager : MonoBehaviour
+	{
+		public static PushNotificationManager instance { get; private set; }
+
+		public string oneSignalAppID;
+
+		void Awake ()
+		{
+			if (instance != null)
+				Destroy (this);
+			else
+				instance = this;
+		}
+
+		void Start ()
+		{
+			// Enable line below to enable logging if you are having issues setting up OneSignal. (logLevel, visualLogLevel)
+			// OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.INFO, OneSignal.LOG_LEVEL.INFO);
+
+			OneSignal.StartInit (oneSignalAppID)
+				.HandleNotificationOpened (HandleNotificationOpened)
+				.EndInit ();
+
+			OneSignal.inFocusDisplayType = OneSignal.OSInFocusDisplayOption.Notification;
+
+			// Call syncHashedEmail anywhere in your app if you have the user's email.
+			// This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+			// OneSignal.syncHashedEmail(userEmail);
+		}
+
+		//		 Gets called when the player opens the notification.
+		private static void HandleNotificationOpened (OSNotificationOpenedResult result)
+		{
+		}
+
+		public void PromptForPermmision(){
+			OneSignal.PromptForPushNotificationsWithUserResponse(OneSignal_promptForPushNotificationsReponse);
+
+		}
+
+		private void OneSignal_promptForPushNotificationsReponse(bool accepted) {
+			Debug.Log("OneSignal_promptForPushNotificationsReponse: " + accepted);
+		}
+	}
+}
